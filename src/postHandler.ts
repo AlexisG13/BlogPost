23c7328b75f11e46a446dfb3d6f86c81f3eaee06
Alexis Gomez <00122717@uca.edu.sv>
2019-12-03 15:42:53 -0600

    :recycle: Refactor on post and update handler
    Update or post by providing the arguments on the request body, not the parameters.

--- src/postHandler.ts
+++ src/postHandler.ts
@@ -1,14 +1,16 @@
 import { clientConnection } from './db';
 const client = clientConnection();
-import * as url from 'url';
 import * as querystring from 'querystring';
 
 export async function postHandler(req: any, res: any) {
-    const parsed = url.parse(req.url);
-    const query = querystring.parse(String(parsed.query));
-    await client.query(`INSERT INTO public.post(
+	let data:string[] = [];
+	await req.on('data', (chunk:string) => {
+		data.push(chunk.toString());
+	});
+    const body = querystring.parse(data[0]);
+	await client.query(`INSERT INTO public.post(
         title, content, id_author,created_at)
-        VALUES ('${query['title']}', '${query['content']}',${query['id_author']},CURRENT_DATE);`);
+        VALUES ('${body['title']}', '${body['content']}',${body['id_author']},CURRENT_DATE);`);
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.end('Post posted succesfully!');
 }
--- src/putHandler.ts
+++ src/putHandler.ts
@@ -1,16 +1,18 @@
 import { clientConnection } from './db';
 const client = clientConnection();
-import * as url from 'url';
 import * as querystring from 'querystring';
 const singlePostRegex = /^\/posts\/([1-9]+)/;
 
 export async function putHandler(req: any, res: any) {
 	const postId = String(req.url).match(singlePostRegex);
 	if (postId !== null) {
-		const parsed = url.parse(req.url);
-        const query = querystring.parse(String(parsed.query));
-        await client.query(`UPDATE public.post
-        SET title='${query['title']}', content='${query['content']}'
+		let data: string[] = [];
+		await req.on('data', (chunk: string) => {
+			data.push(chunk.toString());
+		});
+		const body = querystring.parse(data[0]);
+		await client.query(`UPDATE public.post
+        SET title='${body['title']}', content='${body['content']}'
         WHERE id=${postId[1]};`);
 	}
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
