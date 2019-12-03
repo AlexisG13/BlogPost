c473bae246880165150c0d5f3fe67212e7818436
Alexis Gomez <00122717@uca.edu.sv>
2019-12-03 16:09:46 -0600

    :sparkles: Add function to add comments on a post
    Add function to add comments on a post

--- src/postHandler.ts
+++ src/postHandler.ts
@@ -1,16 +1,24 @@
 import { clientConnection } from './db';
 const client = clientConnection();
 import * as querystring from 'querystring';
+const myRegex = /^\/posts\/comments/;
 
 export async function postHandler(req: any, res: any) {
-	let data:string[] = [];
-	await req.on('data', (chunk:string) => {
+	let data: string[] = [];
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	await req.on('data', (chunk: string) => {
 		data.push(chunk.toString());
 	});
-    const body = querystring.parse(data[0]);
-	await client.query(`INSERT INTO public.post(
+	const body = querystring.parse(data[0]);
+	if (myRegex.test(req.url)) {
+		await client.query(`INSERT INTO public.comment(
+			content,id_author,id_post)
+			VALUES ('${body['content']}',${body['id_author']},${body['id_post']})`);
+			res.end('Comment posted succesfully!');
+	} else {
+		await client.query(`INSERT INTO public.post(
         title, content, id_author,created_at)
-        VALUES ('${body['title']}', '${body['content']}',${body['id_author']},CURRENT_DATE);`);
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Post posted succesfully!');
+		VALUES ('${body['title']}', '${body['content']}',${body['id_author']},CURRENT_DATE);`);
+		res.end('Post posted succesfully!');
+	}
 }
